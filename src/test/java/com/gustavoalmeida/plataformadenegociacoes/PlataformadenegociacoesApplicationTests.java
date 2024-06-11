7affdacc991be2d3e1fc1668416e1a3bebb65107
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-06-11 14:25:55 -0300

    docs: Criação do projeto

---
+++ src/test/java/com/gustavoalmeida/plataformadenegociacoes/PlataformadenegociacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoalmeida.plataformadenegociacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformadenegociacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
